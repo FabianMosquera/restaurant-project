b1e3edd29bd97bad706ec0f51b5da9f8a113497f
fabianmosquera <rfmq9530@gmail.com>
2022-05-25 19:49:31 -0500

    ADD restaurants adn fix the login

--- src/pages/Restaurant/index.tsx
+++ src/pages/Restaurant/index.tsx
@@ -1,20 +1,58 @@
 import { Context } from "../../context/context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import getRestaurante from "../../services/firebase";
+import { useNavigate } from "react-router-dom";
 
 function Restaurant(props: any) {
   const { userLogin } = useContext(Context);
-  (async () => {
-    console.log(await getRestaurante());
-  })();
+  const [filtro, setFiltro] = useState("");
+  const [dato, setDato] = useState([{}]);
+  const [datofil, setDatofil] = useState("");
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    //if (!userLogin) return navigate("/Login")
+    const getData = async () => {
+      const restaurante = await getRestaurante();
+      setDato(restaurante)
+        
+    };
+    getData();
+  }, []);
+
+  const handleChange = (e:any)=> {
+    setFiltro(e.target.value)
+    if (e.target.value == ''){
+      setDatofil('')
+    }
+  }
+
+  const handleClick = ()=> {
+    setDatofil(filtro)
+  }
+
   return (
     <>
       <h1>RESTAURANTE:</h1>
       <div className="Barra-busqueda">
-        <input type="text" placeholder="buscar restaurante" />
-        <button></button>
+        <input name="buscador" type="text" value={filtro} onChange={handleChange} placeholder="buscar restaurante" />
+        <button onClick={handleClick}>Buscar</button>
       </div>
-      <h2>{userLogin}</h2>
+      <div className="container-restaurant">
+        {dato
+          .filter((item:any)=>datofil != '' ? item.ciudad.toLowerCase()===datofil.toLowerCase() :true )
+          .map(
+          ({ Imagen, Direccion, Reseña, nombre }: any, index: number) => (
+            <div key={index}>
+            <h1 >{nombre}</h1>
+            <h2>{Direccion}</h2>
+            <p>{Reseña}</p>
+            <img src={Imagen} alt="" />
+            </div>
+          )
+        )}
+      </div>
     </>
   );
 }
