232ca61eec0fda7cf444854d67a1d9d6fc0cfa65
fabianmosquera <rfmq9530@gmail.com>
2022-05-25 19:50:28 -0500

    MODIFY function

---
+++ src/services/firebase.ts
@@ -0,0 +1,28 @@
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { collection, getDocs, DocumentData, getFirestore} from 'firebase/firestore/lite';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPP9jUPazh436VvJtMD6n5y_YlppcknC8",
+  authDomain: "restaurant-project-d0516.firebaseapp.com",
+  projectId: "restaurant-project-d0516",
+  storageBucket: "restaurant-project-d0516.appspot.com",
+  messagingSenderId: "816840780591",
+  appId: "1:816840780591:web:033fdb293bd12e8cd28fda",
+  measurementId: "G-JEDNP5H9GS"
+};
+
+// Initialize Firebase
+const app: FirebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+async function getRestaurante(){
+  const data:any =[]
+  const resCol = collection(db, 'restaurante');
+  const resSnapshot = await getDocs(resCol);
+  resSnapshot.docs.map(doc => data.push(doc.data()));
+  return data;
+}
+
+
+export default getRestaurante
